e4cd280a916da5d9a63687e2bb2c27f16e18a3c7
Maruf Bin Salim <marufbinsalim22@gmail.com>
2022-06-10 19:24:02 +0600

    Update GameOfLife.js

--- GameOfLife.js
+++ GameOfLife.js
@@ -2,12 +2,24 @@ let isAnimating = false;
 let isDrawable = true;
 let grid, columns, rows;
 let h, w;
-let buttonStartX = 0, fps = 20, nodeSize = 12, density = 100;
+let buttonStartX = 0, fps = 20, nodeSize = 12, density = 72;
 let currentRow = 0, currentColumn = 0;
-let fpsSlider;
-let randomizeSlider;
+let fpsSlider, randomizeSlider;
+let playPauseButton, actionButton, randomizeButton, emptyButton;
+let buttonContainer, sliderContainer;
 
 
+function getAnimationEmoji() {
+  return `${!isAnimating ? "▶" : " | | "}`;
+}
+
+function getDrawEmoji() {
+  return `${isDrawable ? "✎" : "⎚"}`;
+}
+
+function getRandomButtonEmoji() {
+  return `⋙ ${density}%`;
+}
 
 
 function reload() {
@@ -18,11 +30,14 @@ function windowResized() {
 }
 function toggleAnimation() {
   isAnimating = !isAnimating;
+  playPauseButton.html(getAnimationEmoji());
 }
-function toggleDrawErase() {
+function toggleDraw() {
   isAnimating = false;
   isDrawable = !isDrawable;
+  actionButton.html(getDrawEmoji());
 }
+
 function makeArray(columns, rows) {
   let grid = new Array(columns);
   for (let i = 0; i < grid.length; i++) {
@@ -31,116 +46,101 @@ function makeArray(columns, rows) {
   return grid;
 }
 
-function makeEmpty() {
+function generateEmptyGrid() {
   isAnimating = false;
+  playPauseButton.html(getAnimationEmoji());
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
       grid[i][j] = 0;
     }
   }
 }
-function makeRandom() {
+function generateRandomizedGrid() {
   isAnimating = false;
+  playPauseButton.html(getAnimationEmoji());
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
       let random = Math.floor(Math.random() * 100) + 1;
       let threshold = 100 - density;
-      if (j * nodeSize < 110) grid[i][j] = 0;
-      else if (random > threshold) grid[i][j] = 1;
+      if (random > threshold) grid[i][j] = 1;
       else grid[i][j] = 0;
     }
   }
 }
 function createPlayPauseButton() {
-  button = createButton('Play / Pause');
-  button.position(buttonStartX, 50);
-  button.size(100, 25);
-  button.style('background-color', color(0));
-  button.style('color', color(230));
-  button.style('font-weight', 600);
-  button.style('border-radius: 5px;');
-  button.mousePressed(toggleAnimation);
+  playPauseButton = createButton(getAnimationEmoji());
+  playPauseButton.position(buttonStartX, 50);
+  playPauseButton.size(100, 25);
+  playPauseButton.style('background-color', color(0));
+  playPauseButton.style('color', color(230));
+  playPauseButton.style('font-weight', 600);
+  playPauseButton.style('border-radius: 5px;');
+  playPauseButton.mousePressed(toggleAnimation);
 }
+
 function createActionButton() {
-  button = createButton('Draw / Erase');
-  button.position(buttonStartX + 110, 50);
-  button.size(100, 25);
-  button.style('background-color', color(0));
-  button.style('color', color(230));
-  button.style('font-weight', 600);
-  button.style('border-radius: 5px;');
-  button.mousePressed(toggleDrawErase);
+  actionButton = createButton(getDrawEmoji());
+  actionButton.position(buttonStartX + 110, 50);
+  actionButton.size(100, 25);
+  actionButton.style('background-color', color(0));
+  actionButton.style('color', color(230));
+  actionButton.style('font-weight', 600);
+  actionButton.style('border-radius: 5px;');
+  actionButton.mousePressed(toggleDraw);
 }
+
 function createEmptyButton() {
-  button = createButton('Empty');
-  button.position(buttonStartX, 80);
-  button.size(100, 25);
-  button.style('background-color', color(0));
-  button.style('color', color(230));
-  button.style('font-weight', 600);
-  button.style('border-radius: 5px;');
-  button.mousePressed(makeEmpty);
+  emptyButton = createButton('⦰');
+  emptyButton.position(buttonStartX, 80);
+  emptyButton.size(100, 25);
+  emptyButton.style('background-color', color(0));
+  emptyButton.style('color', color(230));
+  emptyButton.style('font-weight', 600);
+  emptyButton.style('border-radius: 5px;');
+  emptyButton.mousePressed(generateEmptyGrid);
 }
 function createRandomizeButton() {
-  button = createButton('Randomize');
-  button.position(buttonStartX + 110, 80);
-  button.size(100, 25);
-  button.style('background-color', color(0));
-  button.style('color', color(230));
-  button.style('font-weight', 600);
-  button.style('border-radius: 5px;');
-  button.mousePressed(makeRandom);
-}
-function displayText() {
-  stroke(114,137,218);
-  fill(25, 33, 41);
-  rect(0, 0, w, 40);
-  fill(230);
-  let state, action;
-  if (isAnimating) state = 'Playing ';
-  else state = 'Paused ';
-  if (isDrawable) action = 'Draw ';
-  else action = 'Erase ';
-  textStyle(BOLD);
-  strokeWeight(0);
-  textSize(15);
-  text('Status : ' + state + '  |  Action : ' + action, w / 2 - 120, 25);
-  strokeWeight(1);
+  randomizeButton = createButton(getRandomButtonEmoji());
+  randomizeButton.position(buttonStartX + 110, 80);
+  randomizeButton.size(100, 25);
+  randomizeButton.style('background-color', color(0));
+  randomizeButton.style('color', color(230));
+  randomizeButton.style('font-weight', 600);
+  randomizeButton.style('border-radius: 5px;');
+  randomizeButton.mousePressed(generateRandomizedGrid);
 }
 
 
 
 function createButtonContainer() {
-
   let width = 250;
   let xPosition = w / 2 - width / 2;
-  let container = createButton("");
-  container.position(xPosition, 45);
-  container.size(width, 65);
-  container.style('background-color', color(40,43,48));
-  container.style('color', color(230));
-  container.style('font-weight', 600);
-  container.style('border-radius: 5px;');
-
+  buttonContainer = createButton("");
+  buttonContainer.position(xPosition, 45);
+  buttonContainer.size(width, 65);
+  buttonContainer.style('background-color', color(40, 43, 48));
+  buttonContainer.style('color', color(230));
+  buttonContainer.style('font-weight', 600);
+  buttonContainer.style('border-radius: 5px;');
 }
-function createSliderContainer() {
-  
+function createSliders() {
+
   let width = floor(windowWidth) / 3;
   let xPosition = w / 2 - width / 2;
-  let container = createButton("");
-  container.position(xPosition - 10, h-70);
-  container.size(width + 20, 65);
-  container.style('background-color', color(40,43,48));
-  container.style('color', color(230));
-  container.style('font-weight', 600);
-  container.style('border-radius: 5px;');
+  sliderContainer = createButton("");
+  sliderContainer.position(xPosition - 10, h - 70);
+  sliderContainer.size(width + 20, 65);
+  sliderContainer.style('background-color', color(40, 43, 48));
+  sliderContainer.style('color', color(230));
+  sliderContainer.style('font-weight', 600);
+  sliderContainer.style('border-radius: 5px;');
 
 
   fpsSlider = createSlider(1, 60, 60);
   fpsSlider.position(xPosition, h - 30);
   fpsSlider.style('width', `${width}px`);
 
-  randomizeSlider = createSlider(1, 100, 100);
+  randomizeSlider = createSlider(1, 100, density);
   randomizeSlider.position(xPosition, h - 60);
   randomizeSlider.style('width', `${width}px`);
 
@@ -183,48 +183,58 @@ function setup() {
   createPlayPauseButton();
   createActionButton();
   createRandomizeButton();
-  createSliderContainer();
+  createSliders();
   columns = floor(w / nodeSize);
   rows = floor(h / nodeSize);
   grid = makeArray(columns, rows);
-  makeEmpty();
+  generateEmptyGrid();
 }
-function draw() {
 
-  frameRate(fpsSlider.value());
-  density = randomizeSlider.value();
-  background(25, 33, 41);
+
+function executeGridUpdationLogic() {
+
+  let updatedGrid = makeArray(columns, rows);
+
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+
+      let state = grid[i][j];
+      let surroundingNodes = countSurroundingNodes(grid, i, j);
+
+      if (state == 0 && surroundingNodes == 3) updatedGrid[i][j] = 1;
+      else if (state == 1 && (surroundingNodes < 2 || surroundingNodes > 3)) updatedGrid[i][j] = 0;
+      else updatedGrid[i][j] = state;
+    }
+  }
+  grid = updatedGrid;
+}
+
+
+function animateGrid() {
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
       let colPos = i * nodeSize;
       let rowPos = j * nodeSize;
       if (grid[i][j] == 1) {
         fill(62, 76, 89);
-        stroke(114,137,218);
+        stroke(114, 137, 218);
+        stroke(40);
         rect(colPos, rowPos, nodeSize - 1, nodeSize - 1);
       }
     }
   }
+}
 
-  let updatedGrid = makeArray(columns, rows);
-  if (isAnimating) {
-    for (let i = 0; i < columns; i++) {
-      for (let j = 0; j < rows; j++) {
-        let state = grid[i][j];
-        let surroundingNodes = countSurroundingNodes(grid, i, j);
-        if (state == 0 && surroundingNodes == 3) {
-          updatedGrid[i][j] = 1;
-        } else if (state == 1 && (surroundingNodes < 2 || surroundingNodes > 3)) {
-          updatedGrid[i][j] = 0;
-        } else {
-          updatedGrid[i][j] = state;
-        }
-      }
-    }
-    grid = updatedGrid;
-  }
+
+function draw() {
+
+  frameRate(fpsSlider.value());
+  density = randomizeSlider.value();
+  randomizeButton.html(getRandomButtonEmoji());
+  background(25, 33, 41);
+  animateGrid();
+  if (isAnimating) executeGridUpdationLogic();
   updateGridFromInput();
-  displayText();
 }
 
 
